97054a1c0552e4c58e189e9b4798bf326761accd
Sambhawi <sambhawi66@gmail.com>
2024-07-16 09:19:11 +0530

    day2

---
+++ spiral.java
@@ -0,0 +1,35 @@
+class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List <Integer> ans = new ArrayList<>();
+        if(matrix.length==0|| matrix==null)return ans;
+        int row = matrix.length;
+        int col = matrix[0].length;
+        int top = 0,bottom=row-1,left=0,right=col-1;
+        while(top<=bottom && left<=right){
+            for(int i = left ; i<=right;i++){
+                ans.add(matrix[top][i]);
+            }
+            top++;
+           for(int i = top;i<=bottom;i++){
+            ans.add(matrix[i][right]);
+           }
+           right--;
+           if(top<=bottom){
+            for(int i =right;i>=left;i--){
+                ans.add(matrix[bottom][i]);
+            }
+              bottom--;
+           }
+          
+            if(left<=right){
+                for(int i = bottom;i>=top;i--){
+                    ans.add(matrix[i][left]);
+                }
+                 left++;
+            }
+           
+        }
+        
+        return ans;
+    }
+}
